8fd15893ae2314237c628aae7e72118f401d48b5
MatheusStorm <131073492+MatheusStorm@users.noreply.github.com>
2023-09-24 23:22:11 -0300

    Update Matheus-banco.sql

--- BANCO/Matheus-banco.sql
+++ BANCO/Matheus-banco.sql
@@ -1,49 +1,49 @@
-create database receitas_deliciosas
-use receitas_deliciosas
+create database receitas_deliciosas;
+use receitas_deliciosas;
     
-    create table usuario {
+    create table receita (
+        id_receita int not null auto_increment primary key,
+        duracao int not null,
+        titulo varchar(100) not null,
+        modo_de_preparo text not null,
+        data_publicação date not null
+    );
+
+    create table ingrediente (
+        id_ingrediente int not null auto_increment primary key,
+        nome_ingrediente varchar(100) not null,
+        qtde_ingrediente int not null,
+        preco_ingrediente decimal(5,2)
+    );
+
+    create table usuario (
         id_usuario int not null auto_increment primary key,
         nome varchar(100) not null,
         email varchar(100) not null,
         senha varchar(100) not null,
         data_nascimento date not null,
         qtde_receita int not null
-    }
-    create table receita_usuario {
+    );
+    create table receita_usuario (
         id_receita_fk int not null auto_increment primary key,
         id_usuario_fk
-    }
+    );
 
-    create table categoria {
+    create table categoria (
         id_categoria int not null auto_increment primary key,
         hashtag varchar(100),
         categoria int
-    }
-    create table receita_categoria {
+    );
+    create table receita_categor (
         id_receita_fk int not null auto_increment primary key,
         id_categoria_fk
-    }
+    );
     
-    create table dificuldade {
+    create table dificul (
         id_dificuldade int not null auto_increment primary key,
         dificuldade int not null
-    }
-    create table receita_dificuldade {
+    );
+    create table receita_dificuldade (
         id_receita_fk int not null auto_increment primary key,
         id_dificuldade_fk
-    }
-    
-    create table receita {
-        id_receita int not null auto_increment primary key,
-        duracao int not null,
-        titulo varchar(100) not null,
-        modo_de_preparo text not null,
-        data_publicação date not null
-    }
-    
-    create table ingrediente {
-        id_ingrediente int not null auto_increment primary key,
-        nome_ingrediente varchar(100) not null,
-        qtde_ingrediente int not null,
-        preco_ingrediente decimal(5,2)
-    }
+    );
