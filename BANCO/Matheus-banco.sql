b6e072370d2a3b4b0b29152b589f080bf0b4b1ff
MatheusStorm <131073492+MatheusStorm@users.noreply.github.com>
2023-10-09 20:40:46 -0300

    sql

--- BANCO/Matheus-banco.sql
+++ BANCO/Matheus-banco.sql
@@ -1,49 +1,112 @@
-create database receitas_deliciosas;
-use receitas_deliciosas;
-    
-    create table receita (
-        id_receita int not null auto_increment primary key,
-        duracao int not null,
-        titulo varchar(100) not null,
-        modo_de_preparo text not null,
-        data_publicação date not null
-    );
+create database tabela;
+use tabela;
 
-    create table ingrediente (
-        id_ingrediente int not null auto_increment primary key,
-        nome_ingrediente varchar(100) not null,
-        qtde_ingrediente int not null,
-        preco_ingrediente decimal(5,2)
-    );
 
-    create table usuario (
-        id_usuario int not null auto_increment primary key,
-        nome varchar(100) not null,
-        email varchar(100) not null,
-        senha varchar(100) not null,
-        data_nascimento date not null,
-        qtde_receita int not null
+    create table tabela (
+        id_tabela int not null auto_increment primary key,
+        datas date not null,
+        temperatura int(10) unsigned not null
+        humidade int(10) unsigned not null
     );
-    create table receita_usuario (
-        id_receita_fk int not null auto_increment primary key,
-        id_usuario_fk
-    );
+    insert into tabela (datas, temperatura, humidade) values (01-01-2031, 11, 11);
+    insert into tabela (datas, temperatura, humidade) values (02-01-2031, 43, 20);
+    insert into tabela (datas, temperatura, humidade) values (03-01-2031, 34, 11);
+    insert into tabela (datas, temperatura, humidade) values (04-01-2032, 33, 54);
+    insert into tabela (datas, temperatura, humidade) values (05-01-2031, 32, 66);
+    insert into tabela (datas, temperatura, humidade) values (06-01-2031, 21, 47);
+    insert into tabela (datas, temperatura, humidade) values (07-01-2031, 44, 67);
+    insert into tabela (datas, temperatura, humidade) values (08-01-2031, 28, 35);
+    insert into tabela (datas, temperatura, humidade) values (09-01-2031, 22, 45);
+    insert into tabela (datas, temperatura, humidade) values (10-01-2031, 21, 34);
+    insert into tabela (datas, temperatura, humidade) values (11-01-2031, 22, 56);
+    insert into tabela (datas, temperatura, humidade) values (12-01-2031, 23, 34);
+    insert into tabela (datas, temperatura, humidade) values (13-01-2031, 21, 14);
+    insert into tabela (datas, temperatura, humidade) values (14-01-2031, 25, 13);
+    insert into tabela (datas, temperatura, humidade) values (15-01-2031, 26, 56);
+    insert into tabela (datas, temperatura, humidade) values (16-01-2031, 23, 56);
+    insert into tabela (datas, temperatura, humidade) values (17-01-2031, 22, 79);
+    insert into tabela (datas, temperatura, humidade) values (18-01-2031, 21, 78);
+    insert into tabela (datas, temperatura, humidade) values (19-01-2031, 23, 54);
+    insert into tabela (datas, temperatura, humidade) values (20-01-2031, 28, 65);
+    insert into tabela (datas, temperatura, humidade) values (21-01-2031, 29, 15);
+    insert into tabela (datas, temperatura, humidade) values (22-01-2031, 30, 16);
+    insert into tabela (datas, temperatura, humidade) values (23-01-2031, 31, 26);
+    insert into tabela (datas, temperatura, humidade) values (24-01-2031, 20, 26);
+    insert into tabela (datas, temperatura, humidade) values (25-01-2031, 16, 64);
+    insert into tabela (datas, temperatura, humidade) values (26-01-2031, 15, 42);
+    insert into tabela (datas, temperatura, humidade) values (27-01-2031, 17, 54);
+    insert into tabela (datas, temperatura, humidade) values (28-01-2031, 33, 57);
+    insert into tabela (datas, temperatura, humidade) values (29-01-2031, 31, 50);
+    insert into tabela (datas, temperatura, humidade) values (30-01-2031, 14, 34);
+    insert into tabela (datas, temperatura, humidade) values (01-02-2031, 36, 22);
+    insert into tabela (datas, temperatura, humidade) values (02-02-2031, 11, 21);
+    insert into tabela (datas, temperatura, humidade) values (03-02-2031, 21, 13);
+    insert into tabela (datas, temperatura, humidade) values (04-02-2031, 12, 11);
+    insert into tabela (datas, temperatura, humidade) values (05-02-2031, 17, 10);
+    insert into tabela (datas, temperatura, humidade) values (06-02-2031, 18, 154);
+    insert into tabela (datas, temperatura, humidade) values (07-02-2031, 13, 110);
+    insert into tabela (datas, temperatura, humidade) values (08-02-2031, 23, 112);
+    insert into tabela (datas, temperatura, humidade) values (09-02-2031, 22, 125);
+    insert into tabela (datas, temperatura, humidade) values (10-02-2031, 10, 59);
+    insert into tabela (datas, temperatura, humidade) values (11-02-2031, 08, 11);
+    insert into tabela (datas, temperatura, humidade) values (12-02-2031, 03, 71);
+    insert into tabela (datas, temperatura, humidade) values (13-02-2031, 11, 91);
+    insert into tabela (datas, temperatura, humidade) values (14-02-2031, 12, 51);
+    insert into tabela (datas, temperatura, humidade) values (15-02-2031, 33, 76);
+    insert into tabela (datas, temperatura, humidade) values (16-02-2031, 04, 55);
+    insert into tabela (datas, temperatura, humidade) values (17-02-2031, 15, 25);
+    insert into tabela (datas, temperatura, humidade) values (18-02-2031, 06, 76);
+    insert into tabela (datas, temperatura, humidade) values (19-02-2031, 23, 120);
+    insert into tabela (datas, temperatura, humidade) values (20-02-2031, 28, 125);
+    insert into tabela (datas, temperatura, humidade) values (21-02-2031, 16, 100);
+    insert into tabela (datas, temperatura, humidade) values (22-02-2031, 19, 101);
+    insert into tabela (datas, temperatura, humidade) values (23-02-2031, 38, 110);
+    insert into tabela (datas, temperatura, humidade) values (24-02-2031, 40, 116);
+    insert into tabela (datas, temperatura, humidade) values (25-02-2031, 44, 160);
+    insert into tabela (datas, temperatura, humidade) values (26-02-2031, 12, 113);
+    insert into tabela (datas, temperatura, humidade) values (27-02-2031, 15, 135);
+    insert into tabela (datas, temperatura, humidade) values (28-02-2031, 10, 126);
+    insert into tabela (datas, temperatura, humidade) values (29-01-2040, 30, 127);
+    insert into tabela (datas, temperatura, humidade) values (30-02-2041, 19, 116);
+    insert into tabela (datas, temperatura, humidade) values (01-02-2042, 20, 43);
+    insert into tabela (datas, temperatura, humidade) values (01-02-2043, 24, 134);
+    insert into tabela (datas, temperatura, humidade) values (01-02-2044, 24, 12);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2045, 25, 15);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2046, 20, 64);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2047, 27, 98);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2048, 21, 86);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2049, 11, 96);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2050, 14, 84);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2051, 33, 35);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2052, 12, 165);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2053, 08, 46);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2054, 04, 98);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2055, 09, 46);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2056, 13, 75);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2057, 40, 36);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2058, 06, 25);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2059, 15, 26);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2060, 09, 43);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2061, 10, 118);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2062, 16, 25);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2063, 12, 56);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2064, 22, 86);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2065, 30, 89);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2066, 18, 13);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2067, 38, 11);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2068, 34, 23);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2069, 26, 11);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2070, 23, 38);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2071, 14, 12);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2072, 10, 113);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2073, 29, 131);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2073, 31, 77);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2075, 13, 31);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2076, 18, 11);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2077, 40, 114);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2078, 19, 115);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2079, 10, 151);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2080, 09, 116);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2081, 25, 25);
+    insert into tabela (datas, temperatura, humidade) values (01-01-2082, 11, 35);
 
-    create table categoria (
-        id_categoria int not null auto_increment primary key,
-        hashtag varchar(100),
-        categoria int
-    );
-    create table receita_categor (
-        id_receita_fk int not null auto_increment primary key,
-        id_categoria_fk
-    );
-    
-    create table dificul (
-        id_dificuldade int not null auto_increment primary key,
-        dificuldade int not null
-    );
-    create table receita_dificuldade (
-        id_receita_fk int not null auto_increment primary key,
-        id_dificuldade_fk
-    );
