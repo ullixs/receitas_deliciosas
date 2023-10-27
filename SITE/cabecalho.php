b53dc3f7cf0d601315cc2d76801abe2ce463d6ec
MatheusStorm <131073492+MatheusStorm@users.noreply.github.com>
2023-10-27 19:21:48 -0300

    Update cabecalho.php

--- SITE/cabecalho.php
+++ SITE/cabecalho.php
@@ -65,14 +65,52 @@
         </script>
 
         <header>
-            <nav>
-                <ul>
-                    <li><a href="#inicio" class="active">Início</a></li>
-                    <li><a href= receitas.html>Receitas</a></li>
-                    <li><a href=sobrenos.html>Sobre Nós</a></li>
-                    <li><a href="#contato">Contato</a></li>
-                </ul>
-            </nav>
-        </header>
+            <nav class="navbar navbar-expand-lg" style="background-color: #33413d;" data-bs-theme="dark">
+                <div class="container-fluid">
+                  <a class="navbar-brand" href="#"><img src="Imagens/RECEITAS DELICIOSAS.png" alt="Receitas Deliciosas" width="400"></a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarScroll">
+                    <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Início</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Receitas</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Sobre nós</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Usuário</a>
+                      </li>
+                      <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          Link
+                        </a>
+                        <ul class="dropdown-menu">
+                          <li><a class="dropdown-item" href="#">Action</a></li>
+                          <li><a class="dropdown-item" href="#">Another action</a></li>
+                          <li><hr class="dropdown-divider"></li>
+                          <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                      <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                    <nav class="navbar" style="background-color:#33413d;">
+                        <form class="container-fluid justify-content-start">
+                            <button class="btn btn-success me-2" type="button">Login</button>
+                            
+                            <button class="btn btn-outline-success me-2" type="button">Registre-se</button>
+                        </form>
+                      </nav>
+                  </div>
+                </div>
+              </nav>
+    </header>
         
         <img src="Imagens/RECEITAS DELICIOSAS.png" alt="Receitas Deliciosas" width="400">
