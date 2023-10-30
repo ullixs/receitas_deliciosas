13ec6c2093ce3428b533cc26f04e74c7c410b417
MatheusStorm <131073492+MatheusStorm@users.noreply.github.com>
2023-10-30 19:26:53 -0300

    Update paginainicial.php

--- SITE/paginainicial.php
+++ SITE/paginainicial.php
@@ -67,7 +67,7 @@
                     </ul>
                     <form class="d-flex" role="search">
                       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                      <button class="btn btn-outline-success" type="submit">Search</button>
+                      <button class="btn btn-outline-success" type="submit">Pesquisa</button>
                     </form>
                     <nav class="navbar" style="background-color:#33413d;">
                         <form class="container-fluid justify-content-start">
