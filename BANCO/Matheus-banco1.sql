c1e89813fdd3c0d4f1033b0d3169150e9f751765
MatheusStorm <131073492+MatheusStorm@users.noreply.github.com>
2023-11-02 12:08:46 -0300

    BANCO UPTADE

--- BANCO/Matheus-banco1.sql
+++ BANCO/Matheus-banco1.sql
@@ -1,53 +1,55 @@
-create database receitasdeliciosas;
+CREATE database receitasdeliciosas;
 use receitasdeliciosas;
     
-    create table receita (
-        id_receita int not null auto_increment primary key,
-        duracao int not null,
-        titulo varchar(100) not null,
-        modo_de_preparo text not null,
-        data_publicação date not null
+    CREATE TABLE receita (
+        id_receita INT NOT NULL auto_increment primary key,
+        duracao INT NOT NULL,
+        titulo VARCHAR(100) NOT NULL,
+        modo_de_preparo text NOT NULL,
+        data_publicação date NOT NULL
     );
 
-    create table ingrediente (
-        id_ingrediente int not null auto_increment primary key,
-        nome_ingrediente varchar(100) not null,
-        qtde_ingrediente int not null,
+    CREATE TABLE ingrediente (
+        id_ingrediente INT NOT NULL auto_increment primary key,
+        nome_ingrediente VARCHAR(100) NOT NULL,
+        qtde_ingrediente INT NOT NULL,
         preco_ingrediente decimal(5,2)
     );
 
-    create table usuario (
-        id_usuario int not null auto_increment primary key,
-        nome varchar(100) not null,
-        email varchar(100) not null,
-        senha varchar(100) not null,
-        data_nascimento date not null,
-        qtde_receita int not null
+    CREATE TABLE usuario (
+        id_usuario INT NOT NULL auto_increment primary key,
+        nome VARCHAR(100) NOT NULL,
+        email VARCHAR(100) NOT NULL,
+        senha VARCHAR(100) NOT NULL,
+        data_nascimento date NOT NULL,
+        qtde_receita INT NOT NULL
     );
-    create table receita_usuario (
-        id_receita_fk int not null auto_increment primary key,
-        id_usuario_fk int,
-        foreign key (id_receita_fk) references receita(id_receita)
+    CREATE TABLE receita_usuario (
+        id_receita_fk INT NOT NULL auto_increment primary key,
+        id_usuario_fk INT,
+        foreign key (id_receita_fk) references receita(id_receita),
+        foreign key (id_usuario_fk) references receita(id_receita)
     );
 
-    create table categoria (
-        id_categoria int not null auto_increment primary key,
-        hashtag varchar(100),
-        categoria int
+    CREATE TABLE categoria (
+        id_categoria INT NOT NULL auto_increment primary key,
+        hashtag VARCHAR(100),
+        categoria INT
     );
-    create table receita_categor (
-        id_receita_fk int not null auto_increment primary key,
-        id_categoria_fk int,
-        foreign key (id_receita_fk) references receita(id_receita)
+    CREATE TABLE receita_categoria (
+        id_receita_fk INT NOT NULL auto_increment primary key,
+        id_categoria_fk INT,
+        foreign key (id_receita_fk) references receita(id_receita),
+        foreign key (id_categoria_fk) references receita(id_receita)
     );
     
-    create table dificuldade (
-        id_dificuldade int not null auto_increment primary key,
-        dificuldade int not null
+    CREATE TABLE dificuldade (
+        id_dificuldade INT NOT NULL auto_increment primary key,
+        dificuldade INT NOT NULL
     );
-    create table receita_dificuldade (
-        id_receita_fk int not null auto_increment primary key,
-        id_dificuldade_fk int,
-        foreign key (id_receita_fk) references receita(id_receita)
+    CREATE TABLE receita_dificuldade (
+        id_receita_fk INT NOT NULL auto_increment primary key,
+        id_dificuldade_fk INT,
+        foreign key (id_receita_fk) references receita(id_receita),
         foreign key (id_dificuldade_fk) references receita(id_receita)
     );
