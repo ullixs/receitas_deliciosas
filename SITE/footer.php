91131a53528ffcc10edcbd9684dc16ccb8fc96ba
MatheusStorm <131073492+MatheusStorm@users.noreply.github.com>
2023-11-06 21:23:37 -0300

    Delete SITE/footer.php

--- SITE/footer.php
+++
@@ -1,6 +0,0 @@
-    <link rel="stylesheet" href="Visual/visualinicial.css">
-        <footer style="background-color: #33413d;">
-            <p>&copy; 2023 Receitas Deliciosas</p>
-        </footer>
-    </body>
-</html>
