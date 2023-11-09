8410cf9875b4536e5d1aaa656319a4c73329e7ae
MatheusStorm <131073492+MatheusStorm@users.noreply.github.com>
2023-11-09 20:09:08 -0300

    arquivos

---
+++ cabecalho.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Descubra receitas deliciosas para cada ocasião. Receitas de pizza, lasanha e muito mais.">
+    <meta name="keywords" content="receitas, comida, pizza, lasanha, culinária">
+    <title>Receitas Deliciosas</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="Visual/visualinicial.css">
+
+    <style>
+
+        /* Estilos CSS personalizados aqui */
+        body {
+            font-family: Arial, sans-serif;
+        }
+        nav ul {
+            list-style: none;
+            padding: 0;
+        }
+        nav li {
+            display: inline;
+            margin-right: 20px;
+        }
+    </style>
+    </head>
+
+    <body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+        </script>
+        
+        <header>
+            <nav class="navbar navbar-expand-lg" style="background-color: #33413d;" data-bs-theme="dark">
+                <div class="container-fluid">
+                  <a class="navbar-brand" href="#"><img src="Imagens/RECEITAS DELICIOSAS.png" alt="Receitas Deliciosas" width="400"></a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarScroll">
+                    <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Início</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Receitas</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Sobre nós</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Usuário</a>
+                      </li>
+                      <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          Link
+                        </a>
+                        <ul class="dropdown-menu">
+                          <li><a class="dropdown-item" href="#">Action</a></li>
+                          <li><a class="dropdown-item" href="#">Another action</a></li>
+                          <li><hr class="dropdown-divider"></li>
+                          <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                      <button class="btn btn-outline-success" type="submit">Pesquisa</button>
+                    </form>
+                    <nav class="navbar" style="background-color:#33413d;">
+                        <form class="container-fluid justify-content-start">
+                        <button class="btn btn-success me-2" type="button"><a href='Login/login.php'>Login</a></button>
+
+                       <button class="btn btn-success me-2" type="button"><a href='Login/registrar.php'>Resgistrar</a></button>
+                        </form>
+                      </nav>
+                  </div>
+                </div>
+              </nav>
+    </header>
---
+++ main.php
@@ -0,0 +1,24 @@
+    <link rel="stylesheet" href="Visual/visualinicial.css">
+    <main>
+        <section id="inicio" class="hero">
+            <h1>Bem-vindo ao Receitas Deliciosas</h1>
+            <p style="font-size: 150%;">Descubra receitas deliciosas para cada ocasião.</p>
+        </section>
+        <br>
+        <section id="receitas" class="featured-recipes">
+            <h2>Receitas em Destaque</h2>
+            <div class="recipe">
+                <img src="Imagens/pizza.jpg" alt="Pizza">
+                <h3>Pizza Caseira</h3>
+                <p>Uma receita deliciosa de pizza caseira.</p>
+                <b><a href="pizza.html">Ver Receita</a></b>
+            </div>
+            <div class="recipe">
+                <img src="Imagens/lasanha.jpg" alt="Lasanha">
+                <h3>Lasanha de Carne</h3>
+                <p>Uma lasanha de carne incrível.</p>
+                <b><a href="lasanha.html">Ver Receita</a></b>
+            </div>
+            <!-- Adicione mais receitas em destaque aqui -->
+        </section>
+    </main>
