eaa2a5ebe3887503a0ffe080a07c08c95b76263a
ullixs <ullian.silva@etec.sp.gov.br>
2023-11-09 22:22:36 -0300

    rascunho sobrenos inserido

--- SITE/sobrenos.php
+++ SITE/sobrenos.php
@@ -1,3 +1,7 @@
 <?php 
 include 'cabecalho.php';
 ?>
+
+Nós somos representantes do projeto receitasdeliciosas desenvolvido para , apresentar um site como projeto na aula de programação , suporte a banco de dados e sistemas operacionais do curso da etec juscelino Kubitschek , responsaveis pelo projeto 
+Matheus de Jesus Rocha e ullian dos santos silva , estamos cusrsando o segundo modulo do curso de informatica
+
