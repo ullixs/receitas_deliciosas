511452009ab6836b2d8354606e8e89de524a1f3b
MatheusStorm <131073492+MatheusStorm@users.noreply.github.com>
2023-11-13 19:52:02 -0300

    Update cabecalho.php

--- SITE/cabecalho.php
+++ SITE/cabecalho.php
@@ -42,7 +42,7 @@
                   <div class="collapse navbar-collapse" id="navbarScroll">
                     <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                       <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="paginainicial.php">Início</a>
+                        <a class="nav-link active" aria-current="page" href="index.php">Início</a>
                       </li>
                       <li class="nav-item">
                         <a class="nav-link" href="receitas.php">Receitas</a>
