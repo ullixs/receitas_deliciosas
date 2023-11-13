0d63da567a8f7af59ea08d6632d3a010e33ff8cb
MatheusStorm <131073492+MatheusStorm@users.noreply.github.com>
2023-11-13 19:49:41 -0300

    Update cabecalho.php

--- SITE/cabecalho.php
+++ SITE/cabecalho.php
@@ -55,13 +55,13 @@
                       </li>
                       <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                          Link
+                          Outros
                         </a>
                         <ul class="dropdown-menu">
-                          <li><a class="dropdown-item" href="#">Action</a></li>
-                          <li><a class="dropdown-item" href="#">Another action</a></li>
+                          <li><a class="dropdown-item" href="#">Ajuda</a></li>
+                          <li><a class="dropdown-item" href="#">Contate-nos</a></li>
                           <li><hr class="dropdown-divider"></li>
-                          <li><a class="dropdown-item" href="#">Something else here</a></li>
+                          <li><a class="dropdown-item" href="#">Configurações</a></li>
                         </ul>
                       </li>
                     </ul>
