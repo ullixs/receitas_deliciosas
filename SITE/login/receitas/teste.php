ff21152bd02addf053d8d94cd0124f8947deec3e
ullixs <ullian.silva@etec.sp.gov.br>
2023-11-22 20:31:51 -0300

    comecando a tentar a colocar receitas , fazes de teste

---
+++ SITE/login/receitas/teste.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="visual.css">
+    <title>Sua Receita</title>
+</head>
+<body>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">
+                <!-- Vídeo do YouTube incorporado -->
+                <iframe width="100%" height="315" src="https://www.youtube.com/embed/rqSr_zWlQBk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div class="col-md-6">
+                <!-- Texto dos ingredientes e modo de preparo -->
+                <h2>Ingredientes:</h2>
+                <ul>
+                    <li>Ingrediente 1</li>
+                    <li>Ingrediente 2</li>
+                    <!-- Adicione mais ingredientes conforme necessário -->
+                </ul>
+                
+                <h2>Modo de Preparo:</h2>
+                <ol>
+                    <li>Passo 1</li>
+                    <li>Passo 2</li>
+                    <!-- Adicione mais passos conforme necessário -->
+                </ol>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
