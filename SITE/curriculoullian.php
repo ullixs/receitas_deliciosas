f01d71adb091a84dd77766b5f2331803cf2df096
ullixs <ullian.silva@etec.sp.gov.br>
2023-11-25 04:22:16 -0300

    arrumando meu curriculo , coloquei receitas , coloquei uma aba de receitas , falta implementar 2 receitas na aba de receitas

--- SITE/curriculoullian.php
+++ SITE/curriculoullian.php
@@ -1,58 +1,72 @@
 <!DOCTYPE html>
-
 <html lang="pt-br">
 
-<!--HEAD-->
-<head> 
+<!-- HEAD -->
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="styles.css" />
-    <h1>Ullian Dos Santos Silva</h1>
-    <p>
-      R. Marquesa De Santos , 294 - Eldorado / Diadema – SP<br />
-      (11) 94605-1279<br />
-      ullian.silva299@gmail.com
-    </p>
+    <title>Ullian Dos Santos Silva</title>
+</head>
 
+<!-- BODY -->
+<body style="font-family: Arial, sans-serif; line-height: 1.6; margin: 20px;">
+
+    <!-- TÍTULO -->
+    <h1>Ullian Dos Santos Silva</h1>
+
+    <!-- CONTATO -->
+    <address>
+        <h2>Contato</h2>
+        <p>
+            R. Marquesa De Santos, 294 - Eldorado / Diadema – SP<br />
+            (11) 94605-1279<br />
+            ullian.silva299@gmail.com
+        </p>
+    </address>
+
+    <!-- OBJETIVO -->
     <h2>Objetivo Profissional</h2>
     <p>
-      Ingressar como Jovem Aprendiz, desenvolvendo habilidades e crescendo
-      profissionalmente, confiante com dedicação e entusiasmo para o sucesso da
-      empresa. Busco aprender, colaborar e crescer nessa oportunidade de
-      primeiro emprego.
+        Ingressar como Jovem Aprendiz, desenvolvendo habilidades e crescendo profissionalmente, confiante com dedicação e entusiasmo para o sucesso da empresa. Busco aprender, colaborar e crescer nessa oportunidade de primeiro emprego.
     </p>
 
+    <!-- ESCOLARIDADE -->
     <h2>Escolaridade</h2>
     <p>
-      Cursando Ensino Médio 3° Ano.<br />
-      Instituição: Wallace C. Simonsen.<br />
-      Previsão do término: 12/2024.<br />
-      Turno: Manhã.
+        Cursando Ensino Médio 3° Ano.<br />
+        Instituição: Wallace C. Simonsen.<br />
+        Previsão do término: 12/2024.<br />
+        Turno: Manhã.
     </p>
 
     <p>
-      Cursando Curso Técnico De Informática 2° MÓDULO.<br />
-      Instituição: Etec Jucelino Klubitscheck de Oliveira.<br />
-      Previsão do término: 06/2024.<br />
-      Turno: Noturno.
+        Cursando Curso Técnico De Informática 2° MÓDULO.<br />
+        Instituição: Etec Jucelino Klubitscheck de Oliveira.<br />
+        Previsão do término: 06/2024.<br />
+        Turno: Noturno.
     </p>
 
+    <!-- EXPERIÊNCIA PROFISSIONAL -->
     <h2>Experiência Profissional</h2>
     <p>Em busca do primeiro emprego.</p>
 
+    <!-- HABILIDADES -->
     <h2>Habilidades</h2>
     <ul>
-      <li>Facilidade em aprender.</li>
-      <li>Comunicativo.</li>
-      <li>Facilidade em se adaptar com mudanças.</li>
-      <li>Capacidade de solucionar problemas.</li>
-      <li>Trabalho em equipe.</li>
+        <li>Facilidade em aprender.</li>
+        <li>Comunicativo.</li>
+        <li>Facilidade em se adaptar com mudanças.</li>
+        <li>Capacidade de solucionar problemas.</li>
+        <li>Trabalho em equipe.</li>
     </ul>
 
+    <!-- IDIOMAS -->
     <h2>Idiomas</h2>
     <p>Espanhol – Médio.</p>
 
-  <li><a href="javascript: history.go(-1)">Voltar para o sobre nós</a></li>
+    <!-- VOLTAR -->
+    <li><a href="javascript: history.go(-1)">Voltar para o sobre nós</a></li>
 
-  </body>
+    <!-- FIM DO BODY -->
+    </body>
 </html>
