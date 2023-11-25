16ac0f8a6cb135958d6c1cc3523bfae57f95b277
ullixs <ullian.silva@etec.sp.gov.br>
2023-11-25 01:27:40 -0300

    colocando receita CheeseCake

---
+++ SITE/receitas/receita4.php
@@ -0,0 +1,54 @@
+<!-- Conteúdo de receitas/receita1.php -->
+<div class="col-md-6">
+    <!-- Coluna de Ingredientes -->
+    <div class="recipe-container">
+        <h2>Ingredientes:</h2>
+        <ul>
+        <li>200g de Bolacha Wafer </li> 
+<li> 400g de Cream Cheese </li>
+<li> 200g de Leite Condensado </li>
+<li> 250ml de Creme de Leite Fresco </li>
+<li> 15g de Gelatina Sem Sabor Incolor </li>
+<li> 60ml de Água </li>
+<li> Suco de 2 Limões </li>
+<li> Blueberry e Hortelã a Gosto </li>
+        </ul>
+
+    </div>
+</div>
+
+<div class="col-md-6">
+    <!-- Coluna de Modo de Preparo -->
+    <div class="recipe-container">
+        <h2>Modo de Preparo:</h2>
+        <ol>
+        <li> Comece triturando as bolachas wafer e passe-as para uma forma de fundo removível, revestida com papel antiaderente. </li>
+
+<li> Pressione bem a massa de bolacha no fundo. </li>
+
+<li> Agora, em uma tigela, coloque o cream cheese e o leite condensado, misture bem até homogeneizar. </li>
+
+<li> Em outra tigela, coloque o creme de leite fresco, bata bem até virar chantilly ou atingir pontos de picos firmes. </li> 
+
+<li> Feito isso, despeje o chantilly com a mistura de cream cheese, misture muito bem e reserve. </li>
+
+<li> Em uma tigela, coloque a gelatina sem sabor e adicione a água e o suco dos limões, misture tudo muito bem. </li>
+
+<li> Leve para o micro-ondas e deixe por 15 segundos.  </li>
+
+<li> Em seguida, acrescente 2 colheres de sopa do creme que preparamos e misture bem, assim como no vídeo. </li>
+
+<li> Depois, despeje tudo no restante do creme e misture até agregar. </li>
+
+<li> Por fim, despeje na forma com a base de bolacha e espalhe. </li>
+
+<li> Leve para a geladeira e deixe gelar por no mínimo 12 horas. </li>
+
+<li> Após esse tempo, desenforme. </li>
+
+<li> Finalize com blueberry e folhas de hortelã. </li>
+
+<li> Sirva-se. </li>
+        </ol>
+    </div>
+</div>
