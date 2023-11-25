f06010f49a8cc4b285bc959f10569c07ae095726
ullixs <ullian.silva@etec.sp.gov.br>
2023-11-25 00:38:19 -0300

    colando moldes para colocar as receitas

--- SITE/receitas/Lasanha.php
+++ SITE/receitas/Lasanha.php
@@ -20,53 +20,14 @@
             </div>
         </div>
 
+        <!-- Seção onde as receitas são incluídas -->
         <div class="row">
-            <div class="col-md-6">
-                <!-- Coluna de Ingredientes -->
-                <div class="recipe-container">
-                    <h2>Ingredientes:</h2>
-                    <ul>
-                    <li>1 Cebola</li>
-                    <li>1 Colher (Chá bem Cheia) de Sal</li>
-                    <li>1 Colher (Chá) de Orégano </li>
-                    <li> 1 Pitada Generosa de Pimenta do Reino (A Gosto) </li>
-                    <li>1 Colher (Chá) de Páprica Doce </li>
-                    <li> 4 Laranjas </li>
-                    <li>700g de Carne</li> 
-                    </ul>
-                    <h4>Molho Branco:</h4>
-                    <ul>
-                    <li> 2 Colheres (Sopa) de Manteiga sem Sal </li>
-<li> 2 Colheres (Sopa) de Farinha de Trigo </li>
-<li> 1 Litro de Leite </li>
-<li> 1 e ½ Colher (Chá) de Sal </li>
-<li> 1 Pitada Generosa de Pimenta do Reino </li> 
-<li> 1 Pitada de Noz Moscada </li>
-                    </ul>
-                </div>
-            </div>
+            <?php
+            $receita1 = 'receita1.php';
 
-            <div class="col-md-6">
-                <!-- Coluna de Modo de Preparo -->
-                <div class="recipe-container">
-                    <h2>Modo de Preparo:</h2>
-                    <ol>
-                    <li> Em uma frigideira, coloque um fio de azeite e adicione a carne moída, refogue até secar a água. </li>
-
-<li>Tempere com o sal, a pimenta do reino e o orégano, misture e acrescente o molho de tomate,  misture e deixe cozinhar por 3 minutos.</li>
-<li> Reserve ao lado. </li>
-<li> Em outra frigideira, coloque a manteiga sem sal e misture até derreter, adicione a farinha de trigo e cozinhe por 2 minutos. </li>
-<li> Após esse tempo, adicione ao leite aos poucos e vá misturando. </li>
-<li> Tempere com o sal, a pimenta do reino e a noz moscada, misture. </li>
-<li> Agora, para montar a receita, pegue um refratário médio e adicione duas conchas do molho branco que preparamos e espalhe. </li>
-<li> Acrescente 2 pacotes de miojo um ao lado do outro, assim como no vídeo. </li>
-<li> Por cima do miojo, coloque metade da carne moída refogada, 150g de queijo mussarela ralado, cubra com molho branco. </li>
-<li> Adicione +2 pacotes do miojo, adicione o restante da carne moída, +150g de queijo ralado e espalhe. </li>
-<li> Leve ao forno pré-aquecido a 180 graus e deixe assar por 30 minutos. </li>
-<li> Sirva-se.</li>
-                    </ol>
-                </div>
-            </div>
+            include $receita1;
+            echo '<hr>'; // Adiciona uma linha horizontal entre as receitas
+            ?>
         </div>
     </div>
 
