a3af4e1a343ac5fc98879671251a31fad3572f3f
ullixs <ullian.silva@etec.sp.gov.br>
2023-11-25 00:53:19 -0300

    adicionando receita de strogonof de camarao

---
+++ SITE/receitas/Estrogonofecamarao.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="visual.css">
+    <title>Sua Receita</title>
+</head>
+<body>
+    <?php include 'cabecalhoteste.php'; ?>
+
+    <div class="container-fluid">
+        <div class="row justify-content-md-center">
+            <div class="col-md-8">
+                <!-- Vídeo do YouTube incorporado -->
+                <div class="video-container">
+                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/uAd_GqEyc7E" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+
+        <!-- Seção onde as receitas são incluídas -->
+        <div class="row">
+            <?php
+            $receita3 = 'receita3.php';
+
+            include $receita3;
+            echo '<hr>'; // Adiciona uma linha horizontal entre as receitas
+            ?>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ SITE/receitas/receita3.php
@@ -0,0 +1,54 @@
+<!-- Conteúdo de receitas/receita1.php -->
+<div class="col-md-6">
+    <!-- Coluna de Ingredientes -->
+    <div class="recipe-container">
+        <h2>Ingredientes:</h2>
+        <ul>
+ <li>       300g de Camarão Limpo e Pré-Cozido </li>
+<li> ½ Colher (Chá) de Sal </li>
+<li> 1 Pitada de Pimenta do Reino </li>
+<li> ½ Limão </li>
+<li> ½ Cebola Média </li>
+<li> 1 Tomate </li>
+<li> Cheiro Verde a Gosto </li>
+        </ul>
+        <h4>Molho:</h4>
+        <ul>
+       <li> ½ Sachê de Molho de Tomate </li>
+<li> 1 Caixa de Creme de Leite (200g) </li>
+ <li> 1 Copo de Requeijão Cremoso </li>
+<li> ½ Colher (Chá) de Sal </li>
+<li> 1 Pitada de Pimenta do Reino </li>
+        </ul>
+    </div>
+</div>
+
+<div class="col-md-6">
+    <!-- Coluna de Modo de Preparo -->
+    <div class="recipe-container">
+        <h2>Modo de Preparo:</h2>
+        <ol>
+       <li> Em uma tigela, coloque o camarão pré-cozido e já limpos. </li>
+
+<li> Tempere-os com o sal, a pimenta do reino, o suco de limão, misture bem e deixe descansar por 15 minutos.</li>
+
+<li> Corte a cebola em cubinhos e reserve. </li> 
+
+<li> Corte o tomate ao meio, retire todas as sementes e corte em cubinhos. </li>
+
+<li> Agora, em uma frigideira, coloque um fio de azeite, adicione os camarões e frite até dourar. </li>
+
+<li> Apague o fogo e reserve. </li>
+
+<li> Depois, em outra frigideira, coloque um fio de azeite, a cebola picada em cubinhos, frite até dourar. </li>
+
+<li> Adicione o molho de tomate, o creme de leite e o requeijão cremoso, misture tudo muito bem. </li>
+
+<li> Tempere com o sal, a pimenta do reino e misture. </li>
+
+<li> Adicione o camarão que fritamos, o tomate picado e cheiro verde a gosto, misture e deixe cozinhar por +2 minutos. </li>
+ 
+<li> Sirva-se com arroz e batata palha. </li>
+        </ol>
+    </div>
+</div>
