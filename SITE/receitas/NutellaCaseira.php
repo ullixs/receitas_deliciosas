faf998eec522ae7f6a80dce7eaf3ad48dde8a932
ullixs <ullian.silva@etec.sp.gov.br>
2023-11-25 01:34:48 -0300

    colocando receita de nutella

---
+++ SITE/receitas/NutellaCaseira.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="visual.css">
+    <title>Sua Receita</title>
+</head>
+<body>
+    <?php include 'cabecalhoteste.php'; ?>
+
+    <div class="container-fluid">
+        <!-- Seção onde o nome da receita é exibido -->
+        <div class="row justify-content-md-center">
+            <div class="col-md-8">
+                <h1 class="recipe-title">Nutella Caseira</h1>
+            </div>
+        </div>
+
+        <!-- Vídeo do YouTube incorporado -->
+        <div class="row justify-content-md-center">
+            <div class="col-md-8">
+                <div class="video-container">
+                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/eQub1iEkznA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+
+        <!-- Seção onde as receitas são incluídas -->
+        <div class="row">
+            <?php
+            $receita5 = 'receita5.php';
+
+            // Inclua o conteúdo da receita
+            include $receita5;
+            echo '<hr>'; // Adiciona uma linha horizontal entre as receitas
+            ?>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ SITE/receitas/receita5.php
@@ -0,0 +1,35 @@
+<!-- Conteúdo de receitas/receita1.php -->
+<div class="col-md-6">
+    <!-- Coluna de Ingredientes -->
+    <div class="recipe-container">
+        <h2>Ingredientes:</h2>
+        <ul>
+       <li> 200g de Avelã Descascadas </li>
+<li> 2 Colheres (Sopa) de Óleo de Coco </li>
+<li> ½ Xícara (Chá) de Cacau em Pó (Xícara de 240 ml) </li>
+<li> 1 Colher (Sopa) de Essência de Baunilha </li>
+<li> ⅓ Xícara (Chá) de Ado</li>ante em Pó Xilitol </li>
+        </ul>
+
+    </div>
+</div>
+
+<div class="col-md-6">
+    <!-- Coluna de Modo de Preparo -->
+    <div class="recipe-container">
+        <h2>Modo de Preparo:</h2>
+        <ol>
+      <li>  Comece colocando as avelãs em uma assadeira e espalhe bem. </li>
+
+<li> Leve para o forno pré-aquecido a 200 graus e deixe assar por 10 minutos. </li>
+
+<li> Após esse tempo, passe as avelãs para um liquidificador ou processador, adicione o óleo de coco e bata até triturar bem. </li>
+
+<li> Depois adicione o cacau em pó, a essência de baunilha e o adoçante xilitol, bata novamente até homogeneizar. </li>
+
+<li> Passe tudo para um pote com tampa. </li>
+
+<li> Sirva-se. </li>
+        </ol>
+    </div>
+</div>
