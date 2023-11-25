35d8f4823a4aab439097c0646a7d4be13c2aea5b
ullixs <ullian.silva@etec.sp.gov.br>
2023-11-25 03:13:10 -0300

    adicionando o receitas , ja a receitas , lazanha e frango atribuida

--- SITE/receitas.php
+++ SITE/receitas.php
@@ -1,3 +1,41 @@
-<?php 
-include 'cabecalho_receitas.php';
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="visualreceitas.css">
+</head>
+
+<body>
+
+    <?php include 'cabecalho_receitas.php'; ?>
+
+    <br>
+    
+    <div class="container-fluid">
+
+        <!-- Seção onde as fotos e nomes das receitas são exibidos -->
+        <div class="row justify-content-md-center">
+            <?php
+            // Exemplo com duas receitas, adicione conforme necessário
+            $receitas = array(
+                array('Lasanha', 'imagens/lasanha.png', 'receitas/lasanha.php'),
+                array('Frango', 'imagens/frango.png', 'receitas/frangomolho.php')
+            );
+
+            foreach ($receitas as $receita) {
+                echo '<div class="col-md-4">';
+                echo '<a href="' . $receita[2] . '" class="recipe-link">';
+                echo '<img src="' . $receita[1] . '" alt="' . $receita[0] . '" class="recipe-image">';
+                echo '<div class="recipe-name">' . $receita[0] . '</div>';
+                echo '</a>';
+                echo '</div>';
+            }
+            ?>
+        </div>
+    </div>
+
+</body>
+
+</html>
