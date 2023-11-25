bcd4b48f203817aea824e2b9e3199e1146da7147
ullixs <ullian.silva@etec.sp.gov.br>
2023-11-25 04:00:00 -0300

    arrumando o sobre nos

--- SITE/sobrenos.php
+++ SITE/sobrenos.php
@@ -1,64 +1,56 @@
-<?php 
-include 'cabecalho_sobrenos.php';
-?>
- <body>
-  <br>
-      <h1 style="text-align: center;">Receitas Deliciosas</h1>
-    <style>
-      p {
 
-        margin-left: 30px;
-      }
-      h2 {
-        margin-left: 30px;
+<?php include 'cabecalho_sobrenos.php'; ?>
 
-margin-left: 30px;
-}
-    </style>
-      
-      <h2>Objetivo do Projeto</h2>
-      <br>
-      <p>
-        O objetivo deste projeto é criar um espaço onde os amantes da culinária
-        possam explorar novas receitas, aprimorar suas habilidades na cozinha e
-        compartilhar suas próprias criações. Os usuários poderão criar perfis,
-        criar receitas e interagir com as receitas de outros usuários por meio
-        de comentários.
-      </p>
+<!DOCTYPE html>
+<html lang="pt-br">
 
-    <br>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="visualsobrenos.css">
+</head>
 
-      <h2>Público Alvo</h2>
-      <br>
-      <p>
-        O público-alvo é formado por pessoas interessadas em aprender a cozinhar
-        ou expandir suas habilidades culinárias. Isso inclui estudantes, jovens
-        adultos que moram sozinhos, pais e qualquer pessoa que deseje explorar
-        receitas caseiras.
-      </p>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <ul>
-        <li><a href="curriculo ullian.php">Ullian dos Santos</a></li>
-        <li><a href="CurriculoMatheus.php">Matheus Rocha</a></li>
+<body>
 
-    <br> 
-  <li><a href="javascript: history.go(-1)"><-VOLTAR-></a></li>
-      </ul>
-      
-      
-      <?php
-      include 'footer.php';
+    <div class="content">
+        <h1>Receitas Deliciosas</h1>
 
-?>
+        <div class="section">
+            <h2>Objetivo do Projeto</h2>
+            <p>
+                O objetivo deste projeto é criar um espaço onde os amantes da culinária
+                possam explorar novas receitas, aprimorar suas habilidades na cozinha e
+                compartilhar suas próprias criações. Os usuários poderão criar perfis,
+                criar receitas e interagir com as receitas de outros usuários por meio
+                de comentários.
+            </p>
+        </div>
 
-    
-  </body>
+        <div class="section">
+            <h2>Público Alvo</h2>
+            <p>
+                O público-alvo é formado por pessoas interessadas em aprender a cozinhar
+                ou expandir suas habilidades culinárias. Isso inclui estudantes, jovens
+                adultos que moram sozinhos, pais e qualquer pessoa que deseje explorar
+                receitas caseiras.
+            </p>
+        </div>
+
+        <div class="section">
+            <h2>Conheça a Equipe</h2>
+            <ul>
+                <li><a href="curriculo_ullian.php">Ullian dos Santos</a></li>
+                <li><a href="CurriculoMatheus.php">Matheus Rocha</a></li>
+            </ul>
+        </div>
+
+        <div class="back-link">
+            <a href="javascript: history.go(-1)"><- VOLTAR</a>
+        </div>
+    </div>
+
+    <?php include 'footer.php'; ?>
+
+</body>
+
 </html>
-
