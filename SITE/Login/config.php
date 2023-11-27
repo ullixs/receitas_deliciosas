e3f435c2aad49f829cf4b2f160edb2ac21910b0b
ullixs <ullian.silva@etec.sp.gov.br>
2023-11-27 20:11:36 -0300

    banco

--- SITE/Login/config.php
+++ SITE/Login/config.php
@@ -4,14 +4,4 @@ $username = "receitasdeliciosas";
 $password = "amd123";
 $pdo = new PDO($dsn, $username, $password);
     
-    $conexao = new mysqli($dsn,$username,$password,$dbName);
-
- if($conexao->connect_errno)
- {
-     echo "Erro";
- }
- else
- {
-     echo "Conexão efetuada com sucesso";
- }
 ?>
