7f08c9a311feda47dcd3e57915b5a206e43af257
MatheusStorm <131073492+MatheusStorm@users.noreply.github.com>
2023-11-28 00:19:11 -0300

    Organização login

---
+++ SITE/login/config.php
@@ -0,0 +1,16 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'receitasdeliciosas';
+    $dbPassword = 'amd123';
+    $dbName = 'receitasdeliciosas';
+    
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+    if($conexao->connect_errno) {
+        echo "Erro";
+    }
+    else {
+        echo "Conexão efetuada com sucesso";
+    }
+?>
